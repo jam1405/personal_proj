56e6a48f987f1bef620b45f0d0cf9c3ed45ec49e
Jamie Farquharson <114078420+jam1405@users.noreply.github.com>
2024-03-31 15:06:19 -0700

    Laptop Push

--- solar_sim/Scripts/model.py
+++ solar_sim/Scripts/model.py
@@ -5,6 +5,9 @@ import numpy as np
 #3D Vector Class outputting a numpy 3x1 array
 class Vec3():
     def __init__(self, x:float , y:float, z:float) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
         self.vec3 = np.array([x,y,z], dtype= float)
         return
     
@@ -19,8 +22,12 @@ class Vec3():
     
     #Combine two 3D vectors -- for changing velocity
     def add(self, other: Vec3):
-        new = self.vec3 + other.vec3
-        return new
+        newx = self.x + other.x
+        newy = self.y + other.y
+        newz = self.z + other.z
+        
+        newvec = Vec3(newx,newy,newz)
+        return newvec
     
     #Angle from point self --> point other
     def angle_between(self, other: Vec3)-> Vec3:
@@ -32,29 +39,32 @@ class Vec3():
 class Body():
     
     #initalize mass, radius, positions and velocity
-    def __init__(self, mass:float, radius:float, pos : Vec3 , vel :Vec3) -> None:
+    def __init__(self, name: str, mass:float, radius:float, pos : Vec3 , vel :Vec3) -> None:
+        self.name = name
         self.m = mass
         self.r = radius #PLANET RADIUS WILL HAVE TO BE DRASTICALLY HIGHER THAN REALITY TO BE VISIBLE
         self.pos = pos
         self.vel = vel
 
     #Gravitational interactions with other planets
-    def interact(self, other: Body) -> Vec3:
-        grav_force_mag: float = params.cos_const*self.m*other.m/(Vec3.dist(self.pos, other.pos)**2)
-        grav_force_dir: Vec3 = Vec3.angle_between(self.pos,other.pos)
-        grav_force_vec: Vec3 = grav_force_mag*grav_force_dir
+    def interact(self, bodies: list) -> Vec3:
+        grav_force_tot:Vec3 = Vec3(0,0,0)
+        for body in bodies:
+            grav_force_mag_temp: float = params.cos_const*body.m/(Vec3.dist(self.pos, body.pos)**2)
+            grav_force_dir_temp: Vec3 = Vec3.angle_between(self.pos,body.pos)
+            grav_force_vec_temp: Vec3 = grav_force_mag_temp*grav_force_dir_temp
+            grav_force_tot.add(grav_force_vec_temp)
         
         #mass factor removed from grav force to indicated acceleration
-        return grav_force_vec
+        return grav_force_tot
     
     def tick(self):
-        #TODO
+    
         
         self.pos = Vec3.add(self.pos, params.time_step*Body.interact())
 class Simulation:
     
     #Initializing celestial body datasets. Position will be stored as Vec3. Names , masses and radii in lists
-    body_pos: Vec3
     body_name: list =[]
     body_mass: list = []
     body_radius: list = []
@@ -62,19 +72,16 @@ class Simulation:
     #Initialize time
     time: float = 0
     
-    #The Planets list is grouped at first index being different planets, and second index being: 0-name, 1->mass, 2->radius, 3,4,5 -> x,y,z locations
+    #The Planets list is grouped at first index being different planets, and second index being: 0-name, 1->mass, 2->radius, 3,4,5 -> x,y,z locations, 6,7,8 -> init vel
     def __init__(self, bodies: list) -> None:
         self.time: int = 0
-        self.vectors: list[Vec3] = []
+        self.bodies: list[Body] = []
         
         for i in range(0,len(bodies)):
-            self.body_name.append(bodies[i,0])
-            self.body_mass.append(bodies[i,1])
-            self.body_radius.append(bodies[i,2])
-            body_pos = Vec3(bodies[i,3], bodies[i,4], bodies[i,5])
-            self.vectors.append(body_pos)
+            self.bodies.append(Body(bodies[i,0],bodies[i,1],bodies[i,2],Vec3(bodies[i,3],bodies[i,4],bodies[i,5]),Vec3(bodies[i,6],bodies[i,7],bodies[i,8])))
     
+    #Each planet updating 
     def tick(self) -> None:
         self.time += 1
-        for Body in self.vectors:
-            
+        for body in self.bodies:
+            body.interact(self.bodies)
--- solar_sim/Scripts/params.py
+++ solar_sim/Scripts/params.py
@@ -4,27 +4,26 @@ CAMERA_DISTANCE : float = 100
 
 #Physics Settings
 #Units of Distance are AU, and mass is Earth Masses, time is in days
-#g = 9.81 #kg*m/s^2
+
 g = 8.196655424*10**(-26) #M_earth*AU/day^2
 time_step = 0.5 #days
-#cos_const = 6.67430*10**(-11) # N*m^2/kg^2
 cos_const = 1.1904*10**(-19)
 
 #Celestial body data lists in units described above
-#form of: [name str, mass float, display_radius float, x_initial, y_initial, z_initial floats]
-sun_data:list = []
-merc_data:list = []
-ven_data:list = []
-ear_data:list = []
-mars_data:list = []
-jup_data:list = []
-sat_data:list = []
-nept_data:list = []
-uran_data:list =[]
-plut_data:list = []
+#form of: [name str, mass float, display_radius float, x_initial, y_initial, z_initial floats, x,y,z init Vels]
+sun_data:list = ["Sun",]
+merc_data:list = ["Mercury",]
+ven_data:list = ["Venus",]
+ear_data:list = ["Earth",]
+mars_data:list = ["Mars",]
+jup_data:list = ["Jupiter",]
+sat_data:list = ["Saturn",]
+uran_data:list = ["Uranus",]
+nept_data:list =["Neptune",]
+plut_data:list = ["Pluto",]
 
 
 #List of Bodies for input
-bodies_input:list = [sun_data, merc_data, ven_data, ear_data, mars_data, jup_data, sat_data, nept_data, uran_data, plut_data ] 
+bodies_input:list = [sun_data, merc_data, ven_data, ear_data, mars_data, jup_data, sat_data, nept_data, uran_data, plut_data] 
     
 
