14c98f2180601b3f19d80fd5ab30891235d7a60e
Jamie Farquharson <114078420+jam1405@users.noreply.github.com>
2024-03-31 17:23:49 -0700

    More crediting :)

--- solar_sim/Scripts/__main__.py
+++ solar_sim/Scripts/__main__.py
@@ -1,5 +1,9 @@
 
+"__main__.py: Runs the solar simulation scripts from model.py through a visualizer: viewcontroller.py, using inputs from params.py"
+__author__      = "Jamie Farquharson"
+__copyright__   = "No copyright, feel free to use as you please :)"
 
+"Credit to JPL Horizons for initial position and velocity data for the various planets."
 
 def main() -> None:
     #Sim start
